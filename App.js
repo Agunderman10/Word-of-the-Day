90bad5e5c5476f43ddc9aa949c897c7c3ebc986a
agunderman10 <agunderman10@gmail.com>
2020-08-13 11:14:21 -0400

    Set up basic ui. Will add more later

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View, SafeAreaView, TextInput } from "react-native";
+import { Fragment } from 'react';
+import { styles } from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Fragment>
+      <SafeAreaView />
+      <View style={styles.container}>
+        <TextInput style={styles.textBox} value="hello" editable={false}></TextInput>
+        <Text>Open up App.js to start working on your app bro!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textBox: {
+        borderWidth: 3,
+        width: '95%',
+        fontSize: 20,
+        color: 'black',
+        textAlign: 'center',
+    },
+  });
